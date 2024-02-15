3b3b90d776ea0cc8f6a8a0380d26e04d6eccf650
Cristian727 <cristian.gonzalez7@hotmail.com>
2024-02-15 11:42:51 +0100

    carpeta

---
+++ test_fs/update.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+const miobjeto_json = fs.readFileSync("./coche.json", "utf-8")
+const miobjeto = JSON.parse(miobjeto_json)
+
+miobjeto.ruedas = 10
+
+fs.writeFileSync("./coche.json", JSON.stringify(miobjeto, null, 2))
+//fs.appendFileSync("./coche.json", JSON.stringify(miobjeto, "utf-8"))
